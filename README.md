fc0e72a03527299117eda259ec5451be3be1cbbf
Caleb Taylor <aquaductape@gmail.com>
2024-10-29 08:23:40 -0700

    update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Meta Title bug
 
+Client-side issue not server-side
+
 In order to see other Title forever persisting, you must change conditions at least 3 times aka click the buttons to switch titles 3 times
 
 Originally I thought if there are more than two Titles inside JSX component and at least one of those Titles is conditionally rendered, the ancestor Title of that JSX component takes precidence. But not as simple as that.
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { Title } from "@solidjs/meta";
 import { createSignal, Match, Show, Switch } from "solid-js";
 
+// This is client-side issue
+
 // In order to see other Title forever persisting, you must change conditions at least 3 times aka click the button 3 times
 // Originally I thought if there are more than two Titles inside JSX component and at least one of those Titles is conditionally rendered, the ancestor Title of that JSX component takes precidence.
 
